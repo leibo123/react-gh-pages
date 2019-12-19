9d295afd78be94829692f53d59d65cb434dfd383
leon <leonl2@bitbucket.org>
2019-12-18 20:51:23 -0500

    Updates

---
+++ precache-manifest.bb12e7435f19d2000f15d6567de3fac0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c143e513817ff87423a8107f89ee517",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "f7f24177d8f580a04953",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef1612611a4cbfd4beaf",
+    "url": "/react-gh-pages/static/js/2.ef1ab00a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.ef1ab00a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7f24177d8f580a04953",
+    "url": "/react-gh-pages/static/js/main.c1a95f2c.chunk.js"
+  },
+  {
+    "revision": "e1e91e16db01ad10fa13",
+    "url": "/react-gh-pages/static/js/runtime-main.81b86187.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
